18f018efedb72f92d92ccc2e70876daf4b4bf3c3
matthewj1561 <77714715+matthewj1561@users.noreply.github.com>
2022-06-02 12:05:26 -0600

    Added login processes

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const router = express.Router();
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('../swagger-output.json');
+const { requiresAuth } = require('express-openid-connect');
 
 router.use('/api-docs', swaggerUi.serve);
 router.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
 router.use('/journal', require('./journal'));
+
 router.use('/user', require('./user'));
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,12 +2,31 @@ const express = require('express');
 
 // const cors = require('cors');
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 const connect = require('./db/connect');
+const { auth, requiresAuth } = require('express-openid-connect');
+require('dotenv').config();
+
+const config = {
+    authRequired: false,
+    auth0Logout: true,
+    secret: process.env.SECRET,
+    baseURL: process.env.BASE_URL,
+    clientID: process.env.CLIENT_ID,
+    issuerBaseURL: process.env.ISSUER_BASE_URL
+};
 
 // app.use(cors());
 app.use(express.json());
-app.use('/', require('./routes'));
+app.use(auth(config));
+app.get('/', (req, res) => {
+    res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+
+app.get('/profile', requiresAuth(), (req, res) => {
+    res.send(JSON.stringify(req.oidc.user));
+});
+app.use('/', requiresAuth(), require('./routes'));
 
 // app.listen(port, () => {
 //     console.log(`Running on port ${port}`);
@@ -18,7 +37,7 @@ connect.initDb((err) => {
     if (err) {
         console.log(err);
     } else {
-        app.listen(process.env.PORT || 8080);
+        app.listen(process.env.PORT || 3000);
         console.log(`Connected to DB and listening on ${port}`);
     }
 });
